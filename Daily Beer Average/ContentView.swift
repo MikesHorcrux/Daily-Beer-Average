24e03f03260aaca8d219417f3ae9e46bea71eeb7
Mike Van Amburg <vanamburg.mv@gmail.com>
2020-04-16 13:24:20 -0500

    Initial Commit

---
+++ Daily Beer Average/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Daily Beer Average
+//
+//  Created by Mike Van Amburg on 4/16/20.
+//  Copyright © 2020 Mike Van Amburg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
